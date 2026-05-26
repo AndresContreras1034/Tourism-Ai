9ab3d5e5a0954e24cef4910c0a7ef84f7ffe2d63
AndresContreras1034 <contrerandres001@gmail.com>
2026-05-25 20:22:57 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Tourism-Ai/',
+  base: '/', // 👈 IMPORTANTE: dominio propio
 
   plugins: [react()],
 
